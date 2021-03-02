741947e984a121a3c70e81017cf05ce97c08e3cb
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2021-03-02 09:59:22 -0300

    todo list feita

---
+++ js/todos.js
@@ -0,0 +1,54 @@
+let todos = [];
+let pagina = 1;
+let quantidade = 7;
+const loader = document.querySelector(".loading");
+const container = document.querySelector(".container");
+const todoList = document.querySelector(".todos");
+function reloadTodoList(){
+    todoList.innerHTML = '';
+    for (let i = (pagina - 1) * quantidade; i < quantidade * pagina; i++) {
+        const currentTodo = todos[i]; 
+        if(currentTodo){
+            todoList.innerHTML += `
+            <li>
+                <input type="checkbox" ${(currentTodo.completed)? "checked": ""}>
+                <p>${currentTodo.title}</p>
+            </li>`
+        }
+    }
+}
+window.onload = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos').then(e=>e.json());
+    loader.classList.add('hide');
+    container.classList.remove('hide')
+    todos = response;
+    reloadTodoList();
+}
+document.querySelector("#nxt").addEventListener('click', ()=>{
+    const teto = Math.ceil(todos.length / quantidade);
+    if(pagina + 1 <= teto){
+        pagina++
+        reloadTodoList();
+    }else{
+        alert('não tem mais páginas')
+    }
+})
+document.querySelector("#bfr").addEventListener('click', ()=>{
+    const teto = Math.ceil(todos.length / quantidade);
+    if(pagina - 1 != 0){
+        pagina--
+        reloadTodoList();
+    }else{
+        alert('não há paginas anteriores')
+    }
+})
+document.querySelector('#search').addEventListener('click',async ()=>{
+    const pesquisa = document.querySelector('#pesquisa').value;
+    loader.classList.remove('hide');
+    container.classList.add('hide')
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos').then(e=>e.json());
+    loader.classList.add('hide');
+    container.classList.remove('hide');
+    todos = response.filter(e=>e.title.includes(pesquisa));
+    reloadTodoList();
+});
