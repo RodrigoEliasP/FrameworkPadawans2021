686b7163d2b9746bb3b3f255a01609ca117b914f
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2021-03-03 10:31:19 -0300

    final commit

--- js/postagens.js
+++ js/postagens.js
@@ -1,26 +1,27 @@
-let albuns = [];
+let postagens = [];
 let usuarios = [];
 let pagina = 1;
 let quantidade = 7;
 const loader = document.querySelector(".loading");
 const container = document.querySelector(".container");
-const albumList = document.querySelector("#albuns");
-function reloadAlbumList(){
-    albumList.innerHTML = '';
+const postList = document.querySelector("#postagens");
+function reloadPostsList(){
+    postList.innerHTML = '';
     for (let i = (pagina - 1) * quantidade; i < quantidade * pagina; i++) {
-        const currentAlbum = albuns[i]; 
-        if(currentAlbum){
-            const index = usuarios.findIndex(e=> e.id == currentAlbum.userId);
-            albumList.innerHTML += 
+        const currentPost = postagens[i]; 
+        if(currentPost){
+            const index = usuarios.findIndex(e=> e.id == currentPost.userId);
+            postList.innerHTML += 
             `<tr>
                 <td>${usuarios[index].username}</td>
-                <td><p>${currentAlbum.title}</p></td>
+                <td><p>${currentPost.title}</p></td>
+                <td><p>${currentPost.body}</p></td>
                 <td>
                     <button 
-                      onclick="loadModal(${currentAlbum.id})" 
+                      onclick="loadModal(${currentPost.id})" 
                       data-toggle="modal" 
                       data-target="#exampleModal"
-                    >Ver fotos</button>
+                    >Ver comentários</button>
                 </td>
             </tr>`
         }
@@ -31,37 +32,41 @@ async function loadModal(id){
     const modalBody = document.querySelector("#modalbody");
     modalBody.innerHTML = "<i class=\"fa fa-spinner fa-spin\" id=\"modalspin\"></i>";
 
-    const response = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`).then(e=>e.json());
+    const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`).then(e=>e.json());
     let imgString = ''
-    response.forEach(foto => {
-        imgString += `<img src="${foto.thumbnailUrl}">`
+    response.forEach(comentario => {
+        imgString += `
+        <div class="comentarios">
+            <h3>${comentario.name}: ${comentario.email}</h3>
+            <p>${comentario.body}</p>
+        </div>`
     });
     modalBody.innerHTML = imgString;
 }
 window.onload = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/albums').then(e=>e.json());
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts').then(e=>e.json());
     const users = await fetch('https://jsonplaceholder.typicode.com/users').then(e=>e.json());
     loader.classList.add('hide');
     container.classList.remove('hide')
-    albuns = response;
+    postagens = response;
     usuarios = users;
-    reloadAlbumList();
+    reloadPostsList();
 }
 document.querySelector("#nxt").addEventListener('click', ()=>{
-    const teto = Math.ceil(albuns.length / quantidade);
+    const teto = Math.ceil(postagens.length / quantidade);
     if(pagina + 1 <= teto){
         pagina++
-        reloadAlbumList();
+        reloadPostsList();
     }else{
         alert('não tem mais páginas')
     }
 })
 
 document.querySelector("#bfr").addEventListener('click', ()=>{
-    const teto = Math.ceil(albuns.length / quantidade);
+    const teto = Math.ceil(postagens.length / quantidade);
     if(pagina - 1 != 0){
         pagina--
-        reloadAlbumList();
+        reloadPostsList();
     }else{
         alert('não há paginas anteriores')
     }
@@ -70,9 +75,9 @@ document.querySelector('#search').addEventListener('click',async ()=>{
     const pesquisa = document.querySelector('#pesquisa').value;
     loader.classList.remove('hide');
     container.classList.add('hide')
-    const response = await fetch('https://jsonplaceholder.typicode.com/albums').then(e=>e.json());
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts').then(e=>e.json());
     loader.classList.add('hide');
     container.classList.remove('hide');
-    albuns = response.filter(e=>e.title.includes(pesquisa));
-    reloadAlbumList();
+    postagens = response.filter(e=>e.title.includes(pesquisa));
+    reloadPostsList();
 });
